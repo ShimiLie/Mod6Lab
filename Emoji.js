1e0bd0ac20ef71bfd40e84caf2ba8d0253534aff
alie <anthonylie94@gmail.com>
2023-03-01 21:40:37 +0800

    added Emoji Component

---
+++ Emoji.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Emoji extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <>
+                <button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? "😊" : "😍"}
+                </button>
+            </>
+        )
+    }
+}
+
+
+export default Emoji;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ClassGreeting from "./ClassGreeting";
+import Emoji from "./Emoji";
 
     function App() {
         return (
             <>
                 <h1>Module 6 Labwork</h1>
                 <ClassGreeting name="Anthony" />
+                <Emoji />
             </>
         )
     }
