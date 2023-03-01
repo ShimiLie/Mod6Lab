901277da713ef8144a06c21d71529f1c51bb4894
alie <anthonylie94@gmail.com>
2023-03-01 20:32:00 +0800

    greeting component

---
+++ ClassGreeting.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+//Lab6.1 Class component
+
+class ClassGreeting extends React.Component {
+
+    render() {
+        return (
+            <>
+                <p>Hello World</p>
+                <p>Hello World</p>
+            </>
+        )        
+    }
+}
+
+
+export default ClassGreeting;
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import ClassGreeting from "./Classgreeting";
 
     function App() {
         return (
             <>
+                <h1>Module 6 Labwork</h1>
+                <ClassGreeting />
             </>
         )
     }
 
-    ReactDOM.render(
-        <App />,
-        document.getElementById("root")
-    );
+ReactDOM.render(
+     <App />,
+    document.getElementById("root")
+);
